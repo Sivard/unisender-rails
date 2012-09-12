3a661e8aa14dc6ddf2ed80703f92f2ce3b4ca160
freeze <sgribovskiy@aforex.ru>
2012-09-12 16:00:47 +0400

    initial commit

---
+++ lib/unisender-rails.rb
@@ -0,0 +1,7 @@
+require "unisender-rails/version"
+
+module Unisender
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/unisender-rails/version.rb
@@ -0,0 +1,5 @@
+module Unisender
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
